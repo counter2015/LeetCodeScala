6a7badeba091c0ae2ec23f94e93c74d5756875b5
counter2015 <voidcounter@gmail.com>
2019-08-01 17:19:56 +0800

    [update] add new solution.

---
+++ src/main/scala/algorithms/medium/implementation/AddTwoNumbers.scala
@@ -0,0 +1,34 @@
+package algorithms.medium.implementation
+
+import algorithms.struct.ListNode
+
+object AddTwoNumbers {
+
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+    var head = new ListNode(0)
+    var res = head
+    var cin = 0
+    var (la, lb) = (l1, l2)
+    while (la != null && lb != null) {
+      val v = la.x + lb.x + cin
+      head.next = new ListNode(v % 10)
+      cin = v / 10
+      la = la.next
+      lb = lb.next
+      head = head.next
+    }
+
+    var node = if (la == null) lb else la
+    while (node != null && cin > 0) {
+      val v = node.x + cin
+      head.next = new ListNode(v % 10)
+      cin = v / 10
+      node = node.next
+      head = head.next
+    }
+
+    if (cin > 0) head.next = new ListNode(cin)
+    res.next
+  }
+}
+
---
+++ src/test/scala/algorithms/medium/implementation/AddTwoNumbersTest.scala
@@ -0,0 +1,66 @@
+package algorithms.medium.implementation
+
+import algorithms.medium.implementation.AddTwoNumbers.addTwoNumbers
+import algorithms.struct.ListNode
+import org.scalatest.FunSuite
+
+import scala.collection.mutable.ArrayBuffer
+
+class AddTwoNumbersTest extends FunSuite {
+  def build(xs: Array[Int]): ListNode = {
+    var res = new ListNode(0)
+    var head = res
+    for (x <- xs) {
+      head.next = new ListNode(x)
+      head = head.next
+    }
+    res.next
+  }
+
+  def get(l: ListNode): Array[Int] = {
+    val xs = ArrayBuffer.empty[Int]
+    var node = l
+    while (node != null) {
+      xs += node.x
+      node = node.next
+    }
+    xs.toArray
+  }
+
+  test("test0: sample input") {
+    val l1 = build(Array(2, 4, 3))
+    val l2 = build(Array(5, 6, 4))
+    val res = Array(7, 0, 8)
+    assert(
+      get(addTwoNumbers(l1, l2)).zip(res).forall(x => x._1 == x._2)
+    )
+  }
+
+  test("test1: zero input") {
+    val l1 = build(Array(0))
+    val l2 = build(Array(0))
+    val res = Array(0)
+    assert(
+      get(addTwoNumbers(l1, l2)).zip(res).forall(x => x._1 == x._2)
+    )
+  }
+
+  test("test2: add with carry with big element") {
+    val l1 = build(Array(1))
+    val l2 = build(Array.fill(100)(9))
+    val res = Array.fill(101)(0)
+    res(100) = 1
+    assert(
+      get(addTwoNumbers(l1, l2)).zip(res).forall(x => x._1 == x._2)
+    )
+  }
+
+  test("test3: add with carry for multi position carry") {
+    val l1 = build(Array(1, 3, 5, 2, 9))
+    val l2 = build(Array(9, 0, 5, 7))
+    val res = Array(0, 4, 0, 0, 0, 1)
+    assert(
+      get(addTwoNumbers(l1, l2)).zip(res).forall(x => x._1 == x._2)
+    )
+  }
+}
