15da5b6b16683bb106b16290f2d3b73a0e167a28
counter2015 <voidcounter@gmail.com>
2019-08-10 13:32:31 +0800

    [update] add new solution.

---
+++ src/main/scala/algorithms/medium/regex/StringToInteger.scala
@@ -0,0 +1,23 @@
+package algorithms.medium.regex
+
+object StringToInteger {
+
+  /** RunTime Info:
+    * 320 ms, 47 MB
+    *
+    * @param str a crazy input string
+    * @return 0, Integer.MAX_VALUE, Integer.MIN_VALUE or a Integer depends your crazy input
+    */
+  def myAtoi(str: String): Int = {
+    val pattern = " *([+-])?(\\d+).*".r
+
+    str match {
+      case pattern(sign, num) =>
+        val bigInt = if (sign == "-") -BigInt(num) else BigInt(num)
+        if (bigInt > Integer.MAX_VALUE) Integer.MAX_VALUE
+        else if (bigInt < Integer.MIN_VALUE) Integer.MIN_VALUE
+        else bigInt.toInt
+      case _ => 0
+    }
+  }
+}
---
+++ src/test/scala/algorithms/medium/regex/StringToIntegerTest.scala
@@ -0,0 +1,51 @@
+package algorithms.medium.regex
+
+import algorithms.medium.regex.StringToInteger.{myAtoi => solve}
+import org.scalatest.FunSuite
+
+class StringToIntegerTest extends FunSuite {
+
+  test("test0: sample input") {
+    assert(solve("42") == 42)
+  }
+
+  test("test1: sample input") {
+    assert(solve("   -42") == -42)
+  }
+
+  test("test2: sample input") {
+    assert(solve("4193 with words") == 4193)
+  }
+
+  test("test3: sample input") {
+    assert(solve("words and 987") == 0)
+  }
+
+  test("test4: sample input") {
+    assert(solve("-91283472332") == Int.MinValue)
+  }
+
+  test("test5: greater than int max value") {
+    assert(solve("+91283472332") == Int.MaxValue)
+  }
+
+  test("test6: +0") {
+    assert(solve("+0") == 0)
+  }
+
+  test("test7: -0") {
+    assert(solve("-0") == 0)
+  }
+
+  test("test8: 0") {
+    assert(solve("0") == 0)
+  }
+
+  test("test9: leading 0") {
+    assert(solve("-000234512341") == -234512341)
+  }
+
+  test("test10: int min value") {
+    assert(solve("-2147483648") == Int.MinValue)
+  }
+}
