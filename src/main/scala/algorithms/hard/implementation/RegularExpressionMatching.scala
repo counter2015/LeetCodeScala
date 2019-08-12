d726b5308b04977fc3f0d3279062632cadd262bc
counter2015 <voidcounter@gmail.com>
2019-08-12 14:01:15 +0800

    [update] add new solution.

---
+++ src/main/scala/algorithms/hard/implementation/RegularExpressionMatching.scala
@@ -0,0 +1,28 @@
+package algorithms.hard.implementation
+
+object RegularExpressionMatching {
+
+  /** RunTime Info:
+    * 688 ms, 53.4 MB
+    *
+    * @param s target string
+    * @param p patter of regex to match
+    * @return whether `s` is the pattern of `p`
+    */
+  def isMatch(s: String, p: String): Boolean = {
+
+    if (p.length == 0) s.length == 0
+    else if (p.length == 1) s.length == 1 && (s == p || p == ".")
+    else {
+      if (p(1) != '*') {
+        s.length > 0 && (s.head == p.head || p.head == '.') && isMatch(s.tail, p.tail)
+      } else {
+        val a = isMatch(s, p.substring(2))
+        val b = s.length > 0 && ((s.head == p.head || p.head == '.') && isMatch(s.tail, p))
+        a || b
+      }
+    }
+
+  }
+
+}
---
+++ src/test/scala/algorithms/hard/implementation/RegularExpressionMatchingTest.scala
@@ -0,0 +1,35 @@
+package algorithms.hard.implementation
+
+import algorithms.hard.implementation.RegularExpressionMatching.{isMatch => solve}
+import org.scalatest.FunSuite
+
+class RegularExpressionMatchingTest extends FunSuite {
+
+  test("test0: sample test") {
+    assert(!solve("aa", "a"))
+  }
+
+  test("test1: sample test") {
+    assert(solve("aa", "a*"))
+  }
+
+  test("test2: sample test") {
+    assert(solve("ab", ".*"))
+  }
+
+  test("test3: sample test") {
+    assert(!solve("aa", "a"))
+  }
+
+  test("test4: sample input") {
+    assert(solve("aab", "c*a*b"))
+  }
+
+  test("test5: sample input") {
+    assert(!solve("mississippi", "mis*is*p*."))
+  }
+
+  test("test6: aaa a.a") {
+    assert(solve("aaa", "a.a"))
+  }
+}
