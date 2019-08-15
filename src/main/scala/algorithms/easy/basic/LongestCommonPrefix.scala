7552af2d2ffa3f5f64dc87f68f60ccfacc3cb5df
counter2015 <voidcounter@gmail.com>
2019-08-15 18:46:55 +0800

    [update] add notes.

--- src/main/scala/algorithms/easy/basic/LongestCommonPrefix.scala
+++ src/main/scala/algorithms/easy/basic/LongestCommonPrefix.scala
@@ -1,6 +1,13 @@
 package algorithms.easy.basic
 
 object LongestCommonPrefix {
+
+  /** RunTime Info:
+    * 244 ms, 44.8 MB
+    *
+    * @param strs a string array
+    * @return the longest common prefix string the input Array[string]
+    */
   def longestCommonPrefix(strs: Array[String]): String = {
     if (strs.length == 0) return ""
     val length = strs.map(_.length).min
